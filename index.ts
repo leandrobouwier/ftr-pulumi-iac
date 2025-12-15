9aa4773b5682e552f6185b2441eb7983a9f20f2e
Leandro Batista <leandrobouwierphp@gmail.com>
2025-12-14 21:19:33 -0300

    new: configure pulumi

--- index.ts
+++ index.ts
@@ -1,13 +1,25 @@
 import * as aws from "@pulumi/aws";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("primeiro-teste-bucket-rocketseat", {
-    bucket: 'primeiro-teste-bucket-rocketseat',
+const bucket = new aws.s3.Bucket("primeiro-bucket", {
+    bucket: 'primeiro-bucket-pos-full',
     tags: {
         IAC: "true"
     }
 
 });
 
-// Export the name of the bucket
+const ecr = new aws.ecr.Repository("primeiro-ecr", {
+    name: 'primeiro-ecr-pos-full',
+    imageTagMutability: 'IMMUTABLE',
+    tags: {
+        IAC: "true",
+    }
+})
+
 export const bucketName = bucket.id;
+export const bucketRegion = bucket.region;
+export const bucketArn = bucket.arn;
+
+export const ecrName = ecr.name
+export const ecrepositoryUrl = ecr.repositoryUrl
