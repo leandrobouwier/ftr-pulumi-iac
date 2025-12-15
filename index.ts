a8c64d3b965fd1345dec04b64f924248c07d75c6
Leandro Batista <leandrobouwierphp@gmail.com>
2025-12-15 14:49:37 -0300

    remove s3 bucket second

--- index.ts
+++ index.ts
@@ -9,13 +9,6 @@ const firstBucket = new aws.s3.Bucket("primeiro-bucket", {
 
 });
 
-const secondBucket = new aws.s3.Bucket("segundo-bucket", {
-    bucket: 'segundo-bucket-pos-full',
-    tags: {
-        IAC: "true"
-    }
-
-});
 
 const ecr = new aws.ecr.Repository("primeiro-ecr", {
     name: 'primeiro-ecr-pos-full',
@@ -29,9 +22,7 @@ export const firstBucketName = firstBucket.id;
 export const firstBucketRegion = firstBucket.region;
 export const firstBucketArn = firstBucket.arn;
 
-export const secondBucketName = secondBucket.id;
-export const secondBucketRegion = secondBucket.region;
-export const secondBucketArn = secondBucket.arn;
+
 
 export const ecrName = ecr.name
 export const ecrepositoryUrl = ecr.repositoryUrl
